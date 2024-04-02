4460776d0d624f8e9e6474fb3cb79839512b719c
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-04-02 09:56:36 +0000

    primer diseño hecho

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numPalo = Math.floor(Math.random() * 4) + 1;
+  let numCarta = Math.floor(Math.random() * 12);
+  let palos = ["heart", "club", "spade", "diamond"];
+  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 };
