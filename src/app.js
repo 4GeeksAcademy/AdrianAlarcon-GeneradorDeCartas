8d8daf45f8346657868c47db964f7bbb79ecacf7
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-04-02 10:36:30 +0000

    practicamente acabado

--- src/app.js
+++ src/app.js
@@ -6,8 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let numPalo = Math.floor(Math.random() * 4) + 1;
-  let numCarta = Math.floor(Math.random() * 12);
+  let numPalo = Math.floor(Math.random() * 4);
+  let numCarta = Math.floor(Math.random() * 12) + 1;
+
+  crearCarta(numPalo, numCarta);
+};
+
+window.crearCarta = function(palo, numero) {
   let palos = ["heart", "club", "spade", "diamond"];
-  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let paloSeleccionado = palos[palo];
+  let icono = "";
+  switch (palo) {
+    case 0:
+      icono = "♥";
+      break;
+    case 1:
+      icono = "♣";
+      break;
+    case 2:
+      icono = "♠";
+      break;
+    case 3:
+      icono = "♦";
+      break;
+    default:
+      icono = "";
+      break;
+  }
+  let idCarta = document.getElementById("carta");
+  idCarta.innerHTML = `<h1 class="${paloSeleccionado} d-flex ms-4">${icono}</h1> 
+  <h1 class="numero">${numero}</h1>
+  <h1 class="${paloSeleccionado}-turned d-flex me-4">${icono}</h1>`;
 };
